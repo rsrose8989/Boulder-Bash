db21a77f708dc85db76b4e546e0afd9460b78bd5
Robert Rose <rsrose8989@gmail.com>
2021-02-22 15:26:20 -0500

    Initial commit

---
+++ Assets/Scripts/BoulderController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoulderController : MonoBehaviour
+{
+    public GameObject boulder;
+    public GameObject spawnedBoulder;
+
+    // Update is called once per frame
+    void Update()
+    {   
+        //controls spawner movement from left to right
+        if (Input.GetKey("a") && transform.position.x<8)
+        {
+            transform.Translate(0.05f, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x < 8)
+        {
+            transform.Translate(0.05f, 0f, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x>-7)
+        {
+            transform.Translate(-0.05f, 0f, 0f);
+        }
+
+        if (Input.GetKey("d") && transform.position.x > -7)
+        {
+            transform.Translate(-0.05f, 0f, 0f);
+        }
+
+        //when spacebar is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //destroy spawned boulder then create new in spawner location
+            Destroy(spawnedBoulder);
+            spawnedBoulder = Instantiate(boulder);
+            spawnedBoulder.transform.position = transform.position;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/WallSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallSpawner : MonoBehaviour
+{
+    public GameObject wallPrefab;
+    public GameObject spawnedWall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnWall();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //spawns wall when R is pressed
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SpawnWall();
+        }
+    }
+
+    void SpawnWall()
+    {
+        //destroy previous wall then create new in random location
+        Destroy(spawnedWall);
+        spawnedWall = Instantiate(wallPrefab);
+        float randomX = Random.Range(-7f, 8f);
+        spawnedWall.transform.position = new Vector3(randomX, spawnedWall.transform.position.y, spawnedWall.transform.position.z);
+    }
+}
+
